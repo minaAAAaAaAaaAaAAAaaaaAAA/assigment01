2abc60547d77b5bd7e3323cd2dc16665b494a0e2
Amina <zinuramina309@gmail.com>
2025-02-16 15:17:30 +0500

    Add initial beans Dog and Owner implementation

--- assigmnt01/src/main/java/org/example/Owner.java
+++ assigmnt01/src/main/java/org/example/Owner.java
@@ -1,4 +1,26 @@
 package org.example;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Owner {
+    private String name;
+    @Autowired
+    private Dog dog;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+    @Autowired
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
 }
--- assigmnt01/src/main/java/org/example/ProjectConfig.java
+++ assigmnt01/src/main/java/org/example/ProjectConfig.java
@@ -1,4 +1,23 @@
 package org.example;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan(basePackages = "org.example")
 public class ProjectConfig {
+
+    @Bean
+    Dog dog(){
+        Dog dog = new Dog();
+        dog.setName("Napaleon");
+        return dog;
+    }
+
+    @Bean
+    public Owner owner() {
+        Owner owner = new Owner();
+        owner.setName("Aizana");
+        return owner;
+    }
 }
